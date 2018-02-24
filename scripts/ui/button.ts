974b2d69c4d4d96db0104dc9e466fd73af99be06
unknown <>
2018-02-24 09:53:05 -0500

    start scene with bug

--- scripts/ui/button.ts
+++ scripts/ui/button.ts
@@ -1,21 +1,31 @@
-module ui {
+module objects {
     export class Button extends createjs.Bitmap {
-        constructor(imageUrl:string, x:number=0, y:number=0, isCentered=false) {
-            super(imageUrl);
+        // Private Instance Variables
+
+        // Public Properties
+
+        // Constructor
+        constructor(assetManager: createjs.LoadQueue, imageString:string, x:number= 0, y:number = 0 ) {
+            super(assetManager.getResult(imageString));
+
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
 
             this.x = x;
             this.y = y;
-            
-            this.on("mouseover", this.mouseOver);
-            this.on("mouseout", this.mouseOut)
+
+            this.on("mouseover", this._mouseOver);
+            this.on("mouseout", this._mouseOut);
+        }
+        // Private Methods
+        private _mouseOver():void {
+            this.alpha = 0.7;
         }
 
-        public mouseOver():void {
-            // mouseover effect
+        private _mouseOut():void {
+            this.alpha = 1.0;
         }
 
-        public mouseOut():void {
-            // mouseout effect
-        }
+        // Public Methods
     }
 }
